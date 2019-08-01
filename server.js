9f27ead50ac975035f86d76c6755e26f34dda3d2
Glitch (hello-express) <none>
2019-08-01 21:38:41 +0000

    🌕❤️ Checkpoint ./package.json:553280/216 ./server.js:553280/16

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express'),
-      webpack = require('webpack'),
-      webpackConfig = require('./webpack.config'),
-      compiler      = webpack(webpackConfig);	
+const express = require('express');
+      //webpack = require('webpack'),
+      //webpackConfig = require('./webpack.config');
+      //compiler      = webpack(webpackConfig);	
 
 const app = express();
 
 
 app.use(express.static('public'));
 
-app.use(compiler);
+//app.use(compiler);
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
